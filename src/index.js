d11fbcd2ab0d0ca1f528ef0de5bc73d03d3fb54e
OlgaKhlivnenko <85929725+OlgaKhlivnenko@users.noreply.github.com>
2022-10-14 19:27:24 +0300

    feat: add img

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ let data = document.querySelector(".data");
 let celsium = document.querySelector(".unit-celsium");
 let foring = document.querySelector(".unit-foring");
 let temp = document.querySelector(".temp");
+let btnCurrent = document.querySelector(".btn-current");
+let weatherDescrip = document.querySelector(".weather-description");
+
 function onSearch(e) {
   e.preventDefault();
   console.log(input.value);
@@ -28,3 +31,22 @@ data.innerHTML = `${day} ${hour}`;
 celsium.addEventListener("click", onChangeCelsium);
 foring.addEventListener("click", onChangeUnit);
 formSearch.addEventListener("submit", onSearch);
+
+function showCurCity(response){
+  city.innerHTML = response.data.name;
+  temp.innerHTML = Math.round(response.data.main.temp);
+  
+}
+
+function currentPosition(position){
+let lat = position.coords.latitude;
+let lon = position.coords.longitude;
+let apiKey = "7ea133b4bf9e4c6570aa5a98e67e1b6b";
+let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+axios.get(url).then(showCurCity);
+}
+
+
+navigator.geolocation.getCurrentPosition(currentPosition);
+btnCurrent.addEventListener("click", showCurTemerature);
