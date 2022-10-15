8c1e36d470b52a15a0a75bba0f2179f9c092206b
OlgaKhlivnenko <85929725+OlgaKhlivnenko@users.noreply.github.com>
2022-10-15 19:10:21 +0300

    feat:add search city

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // write your code here
 let formSearch = document.querySelector(".formSearch");
-let input = document.querySelector(".searchCity");
+let inputEl = document.querySelector(".searchCity");
 let city = document.querySelector(".city");
-let data = document.querySelector(".data");
+let dataEl = document.querySelector(".data");
 let celsium = document.querySelector(".unit-celsium");
 let foring = document.querySelector(".unit-foring");
 let temp = document.querySelector(".temp");
@@ -11,10 +11,15 @@ let weatherDescripEl = document.querySelector(".weather-description");
 let humidityEl = document.querySelector(".humidity");
 let windEl = document.querySelector(".wind");
 
-function onSearch(e) {
+function onSearch(city){
+  let apiKey = "7ea133b4bf9e4c6570aa5a98e67e1b6b";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(showCurCity);
+}
+function handelSubmit(e) {
   e.preventDefault();
-  console.log(input.value);
-  city.innerHTML = input.value;
+  onSearch(inputEl.value);
+  // city.innerHTML = input.value;
 }
 function onChangeUnit(e) {
   e.preventDefault();
@@ -24,35 +29,45 @@ function onChangeCelsium(e) {
   e.preventDefault();
   temp.innerHTML = 10;
 }
+function showTime(timestamp){
+  let now = new Date();
+  let days = ["Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"];
+  let day = days[now.getDay()];
+  let hours = now.getHours();
+    if(hours < 10){
+      hours = `0${hours}`
+    }
+  let minutes = now.getMinutes();
+    if(minutes < 10){
+      minutes = `0${minutes}`
+    }
+  return `${day} ${hours}:${minutes}`
+}
 
-let now = new Date();
-let day = now.toDateString();
-let hour = now.toLocaleTimeString();
-data.innerHTML = `${day} ${hour}`;
+
 
 function showCurCity(response){
   console.log(response);
   city.innerHTML = response.data.name;
   weatherDescripEl.innerHTML = response.data.weather[0].description;
-  console.log(weatherDescripEl);
+  dataEl.innerHTML = showTime(response.data.dt * 1000);
   temp.innerHTML = Math.round(response.data.main.temp);
   humidityEl.innerHTML = response.data.main.humidity;
   windEl.innerHTML = response.data.wind.speed;
 
 }
 
-function currentPosition(position){
-let lat = position.coords.latitude;
-let lon = position.coords.longitude;
-let apiKey = "7ea133b4bf9e4c6570aa5a98e67e1b6b";
-let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-
-axios.get(url).then(showCurCity);
-}
 
 
-navigator.geolocation.getCurrentPosition(currentPosition);
+
+// navigator.geolocation.getCurrentPosition(currentPosition);
 celsium.addEventListener("click", onChangeCelsium);
 foring.addEventListener("click", onChangeUnit);
-formSearch.addEventListener("submit", onSearch);
+formSearch.addEventListener("submit", handelSubmit);
 // btnCurrent.addEventListener("click", showCurTemerature);
