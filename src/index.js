7d7ba2aad6476e18d1855b3d38d52e024328f166
OlgaKhlivnenko <85929725+OlgaKhlivnenko@users.noreply.github.com>
2022-10-15 20:01:48 +0300

    feat: link measuring

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ let formSearch = document.querySelector(".formSearch");
 let inputEl = document.querySelector(".searchCity");
 let city = document.querySelector(".city");
 let dataEl = document.querySelector(".data");
-let celsium = document.querySelector(".unit-celsium");
+let celsiumEl = document.querySelector(".unit-celsium");
 let foring = document.querySelector(".unit-foring");
 let temp = document.querySelector(".temp");
 let btnCurrent = document.querySelector(".btn-current");
@@ -11,6 +11,9 @@ let weatherDescripEl = document.querySelector(".weather-description");
 let humidityEl = document.querySelector(".humidity");
 let windEl = document.querySelector(".wind");
 let iconEl = document.querySelector("#icon");
+
+let celsium = null;
+
 function onSearch(city){
   let apiKey = "7ea133b4bf9e4c6570aa5a98e67e1b6b";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -22,11 +25,15 @@ function handelSubmit(e) {
 }
 function onChangeUnit(e) {
   e.preventDefault();
-  temp.innerHTML = Math.round((temp.innerHTML * 9) / 5 + 32);
+  celsiumEl.classList.remove("active");
+  foring.classList.add("active");
+  temp.innerHTML = Math.round((celsium * 9) / 5 + 32);
 }
 function onChangeCelsium(e) {
   e.preventDefault();
-  temp.innerHTML = 10;
+  celsiumEl.classList.add("active");
+  foring.classList.remove("active");
+  temp.innerHTML = Math.round(celsium);
 }
 function showTime(timestamp){
   let now = new Date();
@@ -54,7 +61,8 @@ function showCity(response){
   city.innerHTML = response.data.name;
   weatherDescripEl.innerHTML = response.data.weather[0].description;
   dataEl.innerHTML = showTime(response.data.dt * 1000);
-  temp.innerHTML = Math.round(response.data.main.temp);
+  celsium = response.data.main.temp;
+  temp.innerHTML = Math.round(celsium);
   humidityEl.innerHTML = response.data.main.humidity;
   windEl.innerHTML = response.data.wind.speed;
   iconEl.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
@@ -65,7 +73,8 @@ function showCity(response){
 
 
 // navigator.geolocation.getCurrentPosition(currentPosition);
-celsium.addEventListener("click", onChangeCelsium);
+celsiumEl.addEventListener("click", onChangeCelsium);
 foring.addEventListener("click", onChangeUnit);
 formSearch.addEventListener("submit", handelSubmit);
 // btnCurrent.addEventListener("click", showCurTemerature);
+onSearch("Odesa");
