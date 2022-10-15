0722b0ffa4ecaf64599646480b615e4f131789f1
OlgaKhlivnenko <85929725+OlgaKhlivnenko@users.noreply.github.com>
2022-10-15 19:34:09 +0300

    feat: add icon

--- src/index.js
+++ src/index.js
@@ -10,16 +10,15 @@ let btnCurrent = document.querySelector(".btn-current");
 let weatherDescripEl = document.querySelector(".weather-description");
 let humidityEl = document.querySelector(".humidity");
 let windEl = document.querySelector(".wind");
-
+let iconEl = document.querySelector("#icon");
 function onSearch(city){
   let apiKey = "7ea133b4bf9e4c6570aa5a98e67e1b6b";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(url).then(showCurCity);
+  axios.get(url).then(showCity);
 }
 function handelSubmit(e) {
   e.preventDefault();
   onSearch(inputEl.value);
-  // city.innerHTML = input.value;
 }
 function onChangeUnit(e) {
   e.preventDefault();
@@ -50,9 +49,7 @@ function showTime(timestamp){
   return `${day} ${hours}:${minutes}`
 }
 
-
-
-function showCurCity(response){
+function showCity(response){
   console.log(response);
   city.innerHTML = response.data.name;
   weatherDescripEl.innerHTML = response.data.weather[0].description;
@@ -60,7 +57,8 @@ function showCurCity(response){
   temp.innerHTML = Math.round(response.data.main.temp);
   humidityEl.innerHTML = response.data.main.humidity;
   windEl.innerHTML = response.data.wind.speed;
-
+  iconEl.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
+  iconEl.setAttribute("alt", response.data.weather[0].description);
 }
 
 
